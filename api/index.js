38c678be1fa6883b1c1dd608bf67fe7353d6d5e6
Bulletdaddy <vercelspamproject@gmail.com>
2026-02-14 16:18:04 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 
+// CORS headers - Automa ko connection allow karenge
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
   next();
 });
 
+// 👇 YEH IMPORTANT HAI - ROOT ROUTE
 app.get('/', (req, res) => {
   // Stream response enable
   res.writeHead(200, {
@@ -15,14 +17,15 @@ app.get('/', (req, res) => {
   });
   
   const messages = [
-    "🎭👙ズ꧁𝑂𝑌𝐸 𝑇𝐸𝑅𝐸 𝐴𝑁𝐾𝑈-𝑃𝐴𝑁𝐾𝑈 𝐾𝐼 𝐵𝐻𝐸𝑁 𝐾𝐼 𝐶𝐻𝑈𝑇 𝑃𝐴𝑅 𝐿𝑂𝐷𝐸 𝑀𝐴𝑅𝑈 ꧂ズ👙🎭\n👞👞👞👞👞👞👞👞👞👞\n👞👞👞👞👞👞👞👞👞👞\n👞👞👞👞👞👞👞👞👞👞\n👞👞👞👞👞👞👞👞👞👞\n👞👞👞👞👞👞👞👞👞👞\n🎭👙ズ꧁𝑂𝑌𝐸 𝑇𝐸𝑅𝐸 𝐴𝑁𝐾𝑈-𝑃𝐴𝑁𝐾𝑈 𝐾𝐼 𝐵𝐻𝐸𝑁 𝐾𝐼 𝐶𝐻𝑈𝑇 𝑃𝐴𝑅 𝐿𝑂𝐷𝐸 𝑀𝐴𝑅𝑈 ꧂ズ👙🎭\n👞👞👞👞👞👞👞👞👞👞\n👞👞👞👞👞👞👞👞👞👞\n👞👞👞👞👞👞👞👞👞👞\n👞👞👞👞👞👞👞👞👞👞\n👞👞👞👞👞👞👞👞👞👞\n🎭👙ズ꧁𝑂𝑌𝐸 𝑇𝐸𝑅𝐸 𝐴𝑁𝐾𝑈-𝑃𝐴𝑁𝐾𝑈 𝐾𝐼 𝐵𝐻𝐸𝑁 𝐾𝐼 𝐶𝐻𝑈𝑇 𝑃𝐴𝑅 𝐿𝑂𝐷𝐸 𝑀𝐴𝑅𝑈 ꧂ズ👙🎭\n👞👞👞👞👞👞👞👞👞👞\n👞👞👞👞👞👞👞👞👞👞\n👞👞👞👞👞👞👞👞👞👞\n👞👞👞👞👞👞👞👞👞👞\n👞👞👞👞👞👞👞👞👞👞\n🎭👙ズ꧁𝑂𝑌𝐸 𝑇𝐸𝑅𝐸 𝐴𝑁𝐾𝑈-𝑃𝐴𝑁𝐾𝑈 𝐾𝐼 𝐵𝐻𝐸𝑁 𝐾𝐼 𝐶𝐻𝑈𝑇 𝑃𝐴𝑅 𝐿𝑂𝐷𝐸 𝑀𝐴𝑅𝑈 ꧂ズ👙🎭"
+    "🎭👙ズ꧁𝑂𝑌𝐸 𝑇𝐸𝑅𝐸 𝐴𝑁𝐾𝑈-𝑃𝐴𝑁𝐾𝑈 𝐾𝐼 𝐵𝐻𝐸𝑁 𝐾𝐼 𝐶𝐻𝑈𝑇 𝑃𝐴𝑅 𝐿𝑂𝐷𝐸 𝑀𝐴𝑅𝑈 ꧂ズ👙🎭"
   ];
 
   const randomMessage = messages[Math.floor(Math.random() * messages.length)];
   
-  // Stream mein bhej
+  // JSON response bhejo
   res.write(JSON.stringify({ message: randomMessage }));
   res.end();
 });
 
+// 👇 YEH BHI IMPORTANT HAI - VERCELL ke liye
 module.exports = app;
